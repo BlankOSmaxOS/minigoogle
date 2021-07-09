7d745260db820a6c458076c978a300c398db6c9a
BlankOSmaxOS <72861954+BlankOSmaxOS@users.noreply.github.com>
2021-07-09 09:42:27 +0200

    Scan TEST

--- src/main/java/searchengine/MiniGoogle.java
+++ src/main/java/searchengine/MiniGoogle.java
@@ -1,12 +1,17 @@
 package searchengine;
 
+import com.opencsv.CSVParser;
+
 import javax.swing.*;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 import static spark.Spark.*;
 
 public class MiniGoogle {
+    private static String input;
 
     // GUI
 
@@ -14,15 +19,58 @@ public class MiniGoogle {
     public static void main(String[] args) throws IOException {
 
         GUISetup();
+        input = "Call me, MAYBE.";
 
-        get("/", (req, res) -> {
-            return "MiniGoogle by Blank Maximilian, Y"; // TODO: replace X and Y by your name(s)
+
+        File file = new File("C:/Users/Blanco/Documents/_privat/Studium_IMI/2021_03_SoSe/B15_Informatik_3/exercise/Info3_Lab_05/src/main/resources/comics.csv");
+        Scanner scan = new Scanner(file);
+        CSVParser csvParser = new CSVParser();
+
+
+        while(scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] words = csvParser.parseLine(line);
+            //System.out.println(scan.nextLine());
+            /*
+            System.out.println(words[0]);
+            System.out.println("______________");
+            System.out.println(words[1]);
+            System.out.println("______________");
+            System.out.println(words[2]);
+
+
+             */
+
+            if (words[2].equals(input)) {
+                System.out.print("Link: ");
+                System.out.println(words[0]);
+                System.out.print("Titel: ");
+                System.out.println(words[1]);
+                System.out.print("Text: ");
+                System.out.println(words[2]);
+                System.out.println("______________");
+            }
+
+        }
+
+
+        //get("/", (req, res) -> {
+          //  return "MiniGoogle by Blank Maximilian, Y"; // TODO: replace X and Y by your name(s)
+
+
+
+
 
             //TODO: Scanner + Streams
+            //TODO: Create/Parse Lists/Arrays Link , Title, Text
+            //TODO: Tag Input If
+            //TODO: Seach Conditions
+            //TODO: Link/Print Results
 
-            //TODO: Seach Condition
             //String input = seachInput.getText();
             //if (input.matches("a-zA-Z")) {
+
+
 
 
             //TODO: GUI
@@ -30,7 +78,7 @@ public class MiniGoogle {
             // TODO: OPTIONS Case Sensitive etc.
 
 
-        });
+       // });
 
     }
 
