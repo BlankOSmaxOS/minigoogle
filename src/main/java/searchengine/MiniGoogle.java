04e2514b2e458109b127c7461f4b420bad319c50
BlankOSmaxOS <72861954+BlankOSmaxOS@users.noreply.github.com>
2021-07-16 16:35:14 +0200

    Update optik und Casesensitive Whole word

--- src/main/java/searchengine/MiniGoogle.java
+++ src/main/java/searchengine/MiniGoogle.java
@@ -20,8 +20,9 @@ public class MiniGoogle {
     public static void main(String[] args) throws IOException {
 
         GUISetup();
-        input = "fresh";
+        input = "DOES";
         boolean caseSensitiv = false;
+        boolean wholeWord = false;
 
         File file = new File("C:/Users/Blanco/Documents/_privat/Studium_IMI/2021_03_SoSe/B15_Informatik_3/exercise/Info3_Lab_05/src/main/resources/comics.csv");
         Scanner scan = new Scanner(file);
@@ -67,28 +68,60 @@ public class MiniGoogle {
                     }
                 }
 
-
                 if (caseSensitiv == false) {
-                    String inputLowerCase = input.toLowerCase();
-                    String wordsLowerCase = words[2].toLowerCase(Locale.ROOT);
-                    String[] wordsLowerCaseSplit = wordsLowerCase.split(" ");
-                    for (int i = 0; i < wordsLowerCaseSplit.length; i++) {
-                        if (wordsLowerCaseSplit[i].matches(inputLowerCase)) {
-                            System.out.print("Link: ");
-                            System.out.println(words[0]);
-                            System.out.print("Titel: ");
-                            System.out.println(words[1]);
-                            System.out.print("Text: ");
-                            System.out.println(words[2]);
-                            System.out.println("______________");
+                    if (wholeWord == true) {
+                        String inputLowerCase = input.toLowerCase();
+                        String wordsLowerCase = words[2].toLowerCase(Locale.ROOT);
+                        String[] wordsLowerCaseSplit = wordsLowerCase.split(" ");
+                        for (int i = 0; i < wordsLowerCaseSplit.length; i++) {
+                            if (wordsLowerCaseSplit[i].matches(inputLowerCase)) {
+                                System.out.print("Link: ");
+                                System.out.println(words[0]);
+                                System.out.print("Titel: ");
+                                System.out.println(words[1]);
+                                System.out.print("Text: ");
+                                System.out.println(words[2]);
+                                System.out.println("______________");
+                            }
+
                         }
+                    }
+                    if (wholeWord == false) {
+                        String inputLowerCase = input.toLowerCase();
+                        String wordsLowerCase = words[2].toLowerCase(Locale.ROOT);
+                        String[] inputLowerCaseSplit = inputLowerCase.split("");
+                        String[] wordsLowerCaseSplit = wordsLowerCase.split("");
+                        int i = 0;
+                        int size = 0;
+                        for (int j = 0; j < wordsLowerCaseSplit.length; j++) {
+                            if (inputLowerCaseSplit[i].equals(wordsLowerCaseSplit[j])) {
+                                i++;
+                                size++;
+                                if (size == inputLowerCaseSplit.length - 1) {
+                                    System.out.print("Input: ");
+                                    System.out.println(input);
+                                    System.out.print("Link: ");
+                                    System.out.println(words[0]);
+                                    System.out.print("Titel: ");
+                                    System.out.println(words[1]);
+                                    System.out.print("Text: ");
+                                    System.out.println(words[2]);
+                                    System.out.println("______________");
+                                    i = 0;
+                                }
+                            } else {
+                                // System.out.println("nothingvin line " + line);
+                                i = 0;
+                                size = 0;
+                            }
+                        }
+
 
                     }
                 }
             }
         }
     }
-
 
 
     //get("/", (req, res) -> {
